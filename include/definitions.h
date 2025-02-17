75659b5852721342063e37ee611aaaaf9b8dd4f6
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2025-02-17 21:42:49 +0700

    mod

--- include/definitions.h
+++ include/definitions.h
@@ -5,3 +5,12 @@
 // DECLARE ID
 uint16_t id = 1;
 
+// MQTT
+#define WIFI_SSID    "KUWIN-IOT"
+#define WIFI_PASS    ""
+#define MQTT_BROKER  "iot.cpe.ku.ac.th"
+#define MQTT_USER    "b6710504344"  
+#define MQTT_PASS    "watcharawish.o@ku.th"     
+#define TOPIC_PREFIX "b6710504344" 
+
+#define TOPIC_KU_BUS TOPIC_PREFIX "/led/switch"
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,16 @@
 #include <LoRa.h>
 #include "definitions.h"
 #include "smartdelay.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 float lat, lon = 0;
 double alt = 0;
 
 uint16_t counter = 0;
 uint16_t sat = 0;
+
+uint32_t last_publish;
 
 SoftwareSerial GPS_SERIAL(18,17);
 
@@ -20,22 +24,35 @@ TinyGPSPlus gps;
 extern void get_gps();
 extern void combine_packet(int id, float lat, float lon, float alt);
 extern void send_packet(String packet);
+extern void mqtt_callback(char* topic, byte* payload, unsigned int length);
+extern void connect_wifi();
+extern void connect_mqtt();
 
 String packet;
 
 Millis timer(1000);
 
+WiFiClient wifiClient;
+PubSubClient mqtt(MQTT_BROKER, 1883, wifiClient);
+
 void setup() {
     Serial.begin(115200);
     GPS_SERIAL.begin(GPS_SERIAL);
+
 
     if(!LoRa.begin(433E6)) {
         while(1);
     }
     Serial.println("Starting Lora");
+
+
+    connect_wifi();
+    connect_mqtt();
+    last_publish = 0;
 }
 
 void loop() {
+    // mqtt.loop();
     get_gps();
     if(timer) {
         combine_packet(id, lat, lon, alt);
@@ -67,4 +84,34 @@ void send_packet(String packet){
     LoRa.beginPacket();
     LoRa.println(packet);
     LoRa.endPacket();
+}
+
+void connect_wifi() {
+    printf("WiFi MAC address is %s\n", WiFi.macAddress().c_str());
+    printf("Connecting to WiFi %s.\n", WIFI_SSID);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        printf(".");
+        fflush(stdout);
+        delay(500);
+    }
+    printf("\nWiFi connected.\n");
+}
+
+void connect_mqtt() {
+    printf("Connecting to MQTT broker at %s.\n", MQTT_BROKER);
+    if (!mqtt.connect("", MQTT_USER, MQTT_PASS)) {
+        printf("Failed to connect to MQTT broker.\n");
+        for (;;) {} // wait here forever
+    }
+    mqtt.setCallback(mqtt_callback);
+
+    mqtt.subscribe(TOPIC_KU_BUS);
+
+    printf("MQTT broker connected.\n");
+}
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+
 }
